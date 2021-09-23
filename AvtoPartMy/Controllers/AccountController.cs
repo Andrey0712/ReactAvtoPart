2f41ba474861429e74330a43cfd8dfab950898e9
nashemistoart@gmail.com <nashemistoart@gmail.com>
2021-09-23 16:01:10 +0300

    Add bd accountControler

---
+++ AvtoPartMy/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using AvtoPartMy.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AvtoPartMy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        [HttpPost]
+        [Route("register")]
+        public IActionResult Register([FromBody] RegisterViewModels model)
+        {
+            //return BadRequest(new {
+            //    message="Такий користувач уже є!"
+            //});
+            return Ok();
+        }
+    }
+}
